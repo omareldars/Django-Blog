b4e35bdf4c2b54f18ce56721236cda513348b096
ahmedelbaiomy <ahmedelbaiomy40@gmail.com>
2021-06-04 03:01:43 -0700

    log out with toggle button

--- blogs/urls.py
+++ blogs/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('delete/<category_id>', views.delete_category, name='delete-category'),
     path('register/',views.register , name='register'),
     path('login/',views.login_view , name='login'), 
+    path('logout/',auth_views.LogoutView.as_view(template_name="user/logout.html"),name='logout'),
     path("profile/",views.profile , name="profile"),
     path('blocked/',views.blocked,name="blocked"),
     path('newword/', views.add_forbiden_word, name='add-word'),
